ae67fd9bea1c3a8852ecdedbe5629f2a94f7cf89
Kathryn <kathrynmikus@gmail.com>
2025-04-28 20:47:02 -0400

    backend 1 complete

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const apiRouter = require('./api');
+
+router.use('/api', apiRouter);
 
 // // Testing
 // router.get('/hello/world', function(req, res) {
 //   res.cookie('XSRF-TOKEN', req.csrfToken());
 //   res.send('Hello World!');
 // });
+
+// // Testing API
+// router.post('/test', function(req, res) {
+//     res.json({ requestBody: req.body });
+//   });
 
 // Add a XSRF-TOKEN cookie
 router.get("/api/csrf/restore", (req, res) => {
