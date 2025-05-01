8921434e3c0c0f584bdd0be892d45cba04fb6c83
Kathryn <kathrynmikus@gmail.com>
2025-04-30 21:24:53 -0400

    frontend 4 complete

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,8 @@
 import { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
 import { Outlet, createBrowserRouter, RouterProvider } from 'react-router-dom';
-import LoginFormPage from './components/LoginFormPage';
-import SignupFormPage from './components/SignupFormPage';
+// import LoginFormPage from './components/LoginFormPage';
+// import SignupFormModal from './components/SignupFormModal';
 import Navigation from './components/Navigation';
 import * as sessionActions from './store/session';
 
@@ -31,15 +31,15 @@ const router = createBrowserRouter([
       {
         path: '/',
         element: <h1>Welcome!</h1>
-      },
-      {
-        path: "login",
-        element: <LoginFormPage />
-      },
-      {
-        path: "signup",
-        element: <SignupFormPage />
       }
+      // {
+      //   path: "login",
+      //   element: <LoginFormPage />
+      // },
+      // {
+      //   path: "signup",
+      //   element: <SignupFormPage />
+      // }
     ]
   }
 ]);
