bca7288844e25b29107d771881916766b40db251
Kathryn <kathrynmikus@gmail.com>
2025-05-10 13:41:21 -0400

    fixed index.js model for render 2

--- backend/db/models/index.js
+++ backend/db/models/index.js
@@ -2,9 +2,26 @@
 
 const { Sequelize } = require('sequelize');
 const config = require('../../config/database');
-const sequelize = new Sequelize(config[process.env.NODE_ENV || 'development']);
+const env = process.env.NODE_ENV || 'development';
+const dbConfig = config[env];
 
-const syncDatabase = () => sequelize.sync(); // ✅ added
+// Initialize Sequelize instance
+let sequelize;
+
+if (dbConfig.use_env_variable) {
+  // Use DATABASE_URL in production
+  sequelize = new Sequelize(process.env[dbConfig.use_env_variable], dbConfig);
+} else {
+  sequelize = new Sequelize(
+    dbConfig.database,
+    dbConfig.username,
+    dbConfig.password,
+    dbConfig
+  );
+}
+
+// Optional: expose syncDatabase helper
+const syncDatabase = () => sequelize.sync();
 
 // Import models
 const User = require('./01-user')(sequelize);
@@ -24,11 +41,11 @@ const Seat = require('./09-Seat')(sequelize);
 const Vendor = require('./11-Vendor')(sequelize);
 const VendorAttachment = require('./12-VendorAttachment')(sequelize);
 
-// Associate models
+// Bundle models and helpers
 const models = {
   sequelize,
   Sequelize,
-  syncDatabase, // ✅ now safe to export
+  syncDatabase,
   User,
   Event,
   EventParty,
@@ -39,18 +56,76 @@ const models = {
   MoodBoard,
   MoodBoardItem,
   Photo,
-  Seat,
   Song,
   Table,
+  Seat,
   Task,
   Vendor,
   VendorAttachment
 };
 
-Object.values(models).forEach(model => {
+// Set up associations
+Object.values(models).forEach((model) => {
   if (typeof model.associate === 'function') {
     model.associate(models);
   }
 });
 
 module.exports = models;
+
+// 'use strict';
+
+// const { Sequelize } = require('sequelize');
+// const config = require('../../config/database');
+// const sequelize = new Sequelize(config[process.env.NODE_ENV || 'development']);
+
+// const syncDatabase = () => sequelize.sync(); // ✅ added
+
+// // Import models
+// const User = require('./01-user')(sequelize);
+// const Event = require('./02-Event')(sequelize);
+// const EventParty = require('./03-EventParty')(sequelize);
+// const Guest = require('./05-Guest')(sequelize);
+// const GuestAttendee = require('./06-GuestAttendee')(sequelize);
+// const OtherGuest = require('./07-OtherGuest')(sequelize);
+// const Member = require('./04-Member')(sequelize);
+// const Task = require('./10-Task')(sequelize);
+// const MoodBoard = require('./13-MoodBoard')(sequelize);
+// const MoodBoardItem = require('./14-MoodBoardItem')(sequelize);
+// const Photo = require('./16-Photo')(sequelize);
+// const Song = require('./15-Song')(sequelize);
+// const Table = require('./08-Table')(sequelize);
+// const Seat = require('./09-Seat')(sequelize);
+// const Vendor = require('./11-Vendor')(sequelize);
+// const VendorAttachment = require('./12-VendorAttachment')(sequelize);
+
+// // Associate models
+// const models = {
+//   sequelize,
+//   Sequelize,
+//   syncDatabase, // ✅ now safe to export
+//   User,
+//   Event,
+//   EventParty,
+//   Guest,
+//   GuestAttendee,
+//   OtherGuest,
+//   Member,
+//   MoodBoard,
+//   MoodBoardItem,
+//   Photo,
+//   Seat,
+//   Song,
+//   Table,
+//   Task,
+//   Vendor,
+//   VendorAttachment
+// };
+
+// Object.values(models).forEach(model => {
+//   if (typeof model.associate === 'function') {
+//     model.associate(models);
+//   }
+// });
+
+// module.exports = models;
