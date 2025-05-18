4a4c93108c355639c3afde2af65cb45e2d9796c1
Kathryn <kathrynmikus@gmail.com>
2025-05-18 13:55:29 -0400

    primary event migtation 4

--- backend/db/migrations/03-create-primary-event-id.js
+++ backend/db/migrations/03-create-primary-event-id.js
@@ -6,7 +6,7 @@ module.exports = {
     if (process.env.NODE_ENV === 'production') {
       options.schema = process.env.SCHEMA;
     }
-    const table = await queryInterface.describeTable('capstone_schema."Users"');
+    const table = await queryInterface.describeTable("Users", {schema:'capstone_schema'});
 
     if (!('primaryEventId' in table)) {
       await queryInterface.addColumn('Users', 'primaryEventId', {
